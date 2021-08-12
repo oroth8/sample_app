5cf548a577b426f55c2efae9b74d18208065b095
oroth8 <oroth8@comcast.net>
2021-08-12 10:20:29 -0500

    added remember me

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -57,4 +57,8 @@ test "password should be present (nonblank)" do
   assert_not @user.valid?
   end
 
+  test "authenticated? should return false for a user with nil digest" do
+    assert_not @user.authenticated?('')
+    end
+
 end
